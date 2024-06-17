5cade91a9c7c1b4fac85dcf681f17cb1fc7f85f4
Althaf Abraham <bangalthaff1@gmail.com>
2024-06-18 01:35:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,63 +35,63 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
 });
 
 function contactWhatsApp() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk menyewa jasa Anda.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function contactUsViaWhatsApp() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya ingin menghubungi anda dan menanyakan Sesuatu.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function orderFilterScript() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk memesan jasa pasang Filter-Script. Mohon informasi lebih lanjut.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function orderFixBugs() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk menggunakan jasa Anda dalam memperbaiki bug. Mohon informasi lebih lanjut.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function orderTextDraw() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk memesan jasa pemasangan TextDraw Samp. Mohon informasi lebih lanjut.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function orderMapping() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk memesan jasa Pasang Mapping. Mohon informasi lebih lanjut.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function orderFilterScript() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk memesan Filter Script Anda. Mohon informasi lebih lanjut.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function orderTextdraw() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk memesan Textdraw Anda. Mohon informasi lebih lanjut.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
 }
 
 function orderGamemode() {
-    var phoneNumber = '6285921918260'; // Ganti dengan nomor telepon yang diinginkan
+    var phoneNumber = ''; // Ganti dengan nomor telepon yang diinginkan
     var message = 'Halo, saya tertarik untuk memesan Gamemode Anda. Mohon informasi lebih lanjut.'; // Pesan yang ingin dikirim
     var whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
     window.open(whatsappUrl, '_blank');
